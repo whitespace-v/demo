d4f4a10c1485c7796136268b940fa04b122aca25
ArchLinux <host@archlinux>
2025-05-13 22:31:05 +1000

    u

--- server/node_modules/@sequelize/sqlite3/lib/query-generator.d.ts
+++
@@ -1,3 +0,0 @@
-import { SqliteQueryGeneratorTypeScript } from './query-generator-typescript.internal.js';
-
-export class SqliteQueryGenerator extends SqliteQueryGeneratorTypeScript {}
--- server/node_modules/@sequelize/sqlite3/lib/query.d.ts
+++
@@ -1,3 +0,0 @@
-import { AbstractQuery } from '@sequelize/core';
-
-export class SqliteQuery extends AbstractQuery {}
